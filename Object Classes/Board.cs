f70d725d4dfc74f6a1a40a517e51e95012477aa8
ellemelon <gingerellen012@gmail.com>
2018-10-02 09:39:40 +1000

    Initial commit of all code for the project

---
+++ Object Classes/Board.cs
@@ -0,0 +1,174 @@
+﻿using System.Diagnostics;
+
+namespace Object_Classes {
+	/// <summary>
+	/// Models a game board for Space Race consisting of three different types of squares
+	/// 
+	/// Ordinary squares, Wormhole squares and Blackhole squares.
+	/// 
+	/// landing on a Wormhole or Blackhole square at the end of a player's move 
+	/// results in the player moving to another square
+	/// 
+	/// </summary>
+	/// 
+
+
+	public static class Board {
+		/// <summary>
+		/// Models a game board for Space Race consisting of three different types of squares
+
+		/// Ordinary squares, Wormhole squares and Blackhole squares.
+		/// 
+		/// landing on a Wormhole or Blackhole square at the end of a player's move 
+		/// results in the player moving to another square
+		/// 
+		/// 
+		/// </summary>
+
+		public const int NUMBER_OF_SQUARES = 56;
+		public const int START_SQUARE_NUMBER = 0;
+		public const int FINISH_SQUARE_NUMBER = NUMBER_OF_SQUARES - 1;
+
+		private static Square[] squares = new Square[NUMBER_OF_SQUARES];
+
+		static int status = 0; // store the status of the squares 1 = normal 2= blackhole and 3=wormhole
+
+		// static int[] board = new int[NUMBER_OF_SQUARES];
+
+
+		public static Square[] Squares {
+			get {
+				Debug.Assert(squares != null, "squares != null",
+				   "The game board has not been instantiated");
+				return squares;
+			}
+		}
+
+		public static Square StartSquare {
+			get {
+				return squares[START_SQUARE_NUMBER];
+			}
+		}
+
+
+		/// <summary>
+		///  Eight Wormhole squares.
+		///  
+		/// Each row represents a Wormhole square number, the square to jump forward to and the amount of fuel consumed in that jump.
+		/// 
+		/// For example {2, 22, 10} is a Wormhole on square 2, jumping to square 22 and using 10 units of fuel
+		/// 
+		/// </summary>
+		private static int[,] wormHoles =
+		{
+			{2, 22, 10},
+			{3, 9, 3},
+			{5, 17, 6},
+			{12, 24, 6},
+			{16, 47, 15},
+			{29, 38, 4},
+			{40, 51, 5},
+			{45, 54, 4}
+		};
+
+		/// <summary>
+		///  Eight Blackhole squares.
+		///  
+		/// Each row represents a Blackhole square number, the square to jump back to and the amount of fuel consumed in that jump.
+		/// 
+		/// For example {10, 4, 6} is a Blackhole on square 10, jumping to square 4 and using 6 units of fuel
+		/// 
+		/// </summary>
+		private static int[,] blackHoles =
+		{
+			{10, 4, 6},
+			{26, 8, 18},
+			{30, 19, 11},
+			{35,11, 24},
+			{36, 34, 2},
+			{49, 13, 36},
+			{52, 41, 11},
+			{53, 42, 11}
+		};
+
+
+		/// <summary>
+		/// Parameterless Constructor
+		/// Initialises a board consisting of a mix of Ordinary Squares,
+		///     Wormhole Squares and Blackhole Squares.
+		/// 
+		/// Pre:  none
+		/// Post: board is constructed
+		/// </summary>
+		/// 
+		//private static int[] board = {1,4,6,7,8,9,11,13,14,15,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,37,38,39,41,42,43,44,46,47,48,50,51,54,55,56, };  // normal squares stops at 56 (including 56)
+		private static int[] board = new int[NUMBER_OF_SQUARES];
+		static Board() {
+
+			for (int i = 0; i < 55; i++) {
+				board [i] = i;
+			}
+
+		}
+
+	
+	public static void SetUpBoard() {
+
+            // Create the 'start' square where all players will start.
+            squares[START_SQUARE_NUMBER] = new Square("Start", START_SQUARE_NUMBER);
+
+			for (int i = 1; i<55; i++)
+			{
+				for (int j = 0; j<8; j++)
+				{
+					if (i == blackHoles[j, 0])
+					{
+						squares[i] = new BlackholeSquare("blackhole", i, blackHoles[j, 1], blackHoles[j, 2]);
+					}
+
+					if (i == wormHoles[j, 0])
+					{
+						squares[i] = new WormholeSquare("wormhole", i, wormHoles[j, 1], wormHoles[j, 2]);
+					}
+
+					else
+					{
+						squares[i] = new Square("ordinary", i);
+					}
+				}
+			}
+
+            // Create the main part of the board, squares 1 .. 54
+            //  CODE NEEDS TO BE ADDED HERE
+            //
+            //   Need to call the appropriate constructor for each square
+            //       either new Square(...),  new WormholeSquare(...) or new BlackholeSquare(...)
+            //
+
+            // Create the 'finish' square.
+            squares[FINISH_SQUARE_NUMBER] = new Square("Finish", FINISH_SQUARE_NUMBER);
+        } // end SetUpBoard
+
+        /// <summary>
+        /// Finds the destination square and the amount of fuel used for either a 
+        /// Wormhole or Blackhole Square.
+        /// 
+        /// pre: squareNum is either a Wormhole or Blackhole square number
+        /// post: destNum and amount are assigned correct values.
+        /// </summary>
+        /// <param name="holes">a 2D array representing either the Wormholes or Blackholes squares information</param>
+        /// <param name="squareNum"> a square number of either a Wormhole or Blackhole square</param>
+        /// <param name="destNum"> destination square's number</param>
+        /// <param name="amount"> amont of fuel used to jump to the deestination square</param>
+        private static void FindDestSquare(int[,] holes, int squareNum, out int destNum, out int amount) {
+            const int start = 0, exit = 1, fuel = 2;
+            destNum = 0; amount = 0;
+
+            //  CODE NEEDS TO BE ADDED HERE 
+
+        } //end FindDestSquare
+
+
+
+    } //end class Board
+}
---
+++ Object Classes/Player.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace Object_Classes {
+    /// <summary>
+    /// A player who is currently located  on a particular square 
+    ///   with a certain amount of rocket fuel remaining
+    /// </summary>
+    public class Player {
+        public const int INITIAL_FUEL_AMOUNT = 60;
+
+        // name of the player
+        private string name;
+        public string Name {
+            get {
+                return name;
+            }
+            set {
+                name = value;
+            }
+        }
+        //position on board
+        private int position;
+        public int Position {
+            get {
+                return position;
+            }
+
+            set {
+                position = value;
+            }
+        }
+
+        // current square that player is on
+        private Square location;
+        public Square Location {
+            get {
+                return location;
+            }
+            set {
+                location = value;
+            }
+        }
+
+
+        //this has been added
+        //the number of squares the player will move
+        private int squaresToMove;
+        public int SquaresToMove {
+            get {
+                return squaresToMove;
+            }
+            set {
+                squaresToMove = value;
+            }
+        }
+
+
+        // amount of rocket fuel remaining for this player
+        private int fuelLeft;
+        public int RocketFuel {
+            get {
+                return fuelLeft;
+            }
+            set {
+                fuelLeft = value;
+            }
+        }
+
+        // true if fuelLeft > 0
+        // otherwise false
+        private bool hasPower;
+        public bool HasPower {
+            get {
+                return hasPower;
+            }
+            set {
+                hasPower = value;
+            }
+        }
+
+        private bool atFinish;
+        public bool AtFinish {
+            get {
+                return atFinish;
+            }
+            set {
+                atFinish = value;
+            }
+        }
+
+        private Brush playerTokenColour;
+        public Brush PlayerTokenColour {
+            get {
+                return playerTokenColour;
+            }
+            set {
+                playerTokenColour = value;
+                playerTokenImage = new Bitmap(1, 1);
+                using (Graphics g = Graphics.FromImage(PlayerTokenImage))
+                {
+                    g.FillRectangle(playerTokenColour, 0, 0, 1, 1);
+                }
+            }
+        }
+
+        private Image playerTokenImage;
+        public Image PlayerTokenImage {
+            get {
+                return playerTokenImage;
+            }
+        }
+
+        
+        /// <summary>
+        /// Parameterless constructor.
+        /// Do not want the generic default constructor to be used
+        /// as there is no way to set the player's name.
+        /// This overwrites the compiler's generic default constructor.
+        /// Pre:  none
+        /// Post: ALWAYS throws an ArgumentException.
+        /// </summary>
+        /// <remarks>NOT TO BE USED!</remarks>
+        public Player() {
+            throw new ArgumentException("Parameterless constructor invalid.");
+        } // end Player constructor
+
+        /// <summary>
+        /// Constructor with initialising parameters.
+        /// Pre:  name to be used for this player.
+        /// Post: player object has name
+        /// </summary>
+        /// <param name="name">Name for this player</param>
+        public Player(String name)//, Square initialLocation)
+        {
+            Name = name;
+        } // end Player constructor
+
+       
+        /// <summary>
+        /// Rolls the two dice to determine 
+        ///     the number of squares to move forward;
+        ///     moves the player position on the board; 
+        ///     updates the player's location to new position; and
+        ///     determines the poutcome of landing on this square.
+        /// Pre: the dice are itialised
+        /// Post: the player is moved along the board and the effect
+        ///     of the location the player landed on is applied.
+        /// </summary>
+        /// <param name="d1">first die</param>
+        /// <param name="d2">second die</param>
+        public void Play(Die d1, Die d2) {
+
+            //roll the two dice and assign to number of squares to move
+            SquaresToMove = d1.FaceValue + d2.FaceValue;
+
+            //move the player position on the board
+          
+            Position = Position + SquaresToMove;
+
+            //update the players location to new position
+            Location = Board.Squares[Position];
+
+            //determine the outcome of landing on this square
+            //some if statements based on the current Location of the player
+            //and whether or not they need to move somewhere else or not
+
+            //this is a really gross way to do it...
+
+            if (Location.Name == "WormholeSquare") {
+                //go to location
+                Location. = 
+                //fuel change
+                if (RocketFuel == INITIAL_FUEL_AMOUNT) {
+                    RocketFuel = INITIAL_FUEL_AMOUNT - ;
+                } else {
+                    RocketFuel = RocketFuel - ;
+                }
+                
+
+            } else {
+                if (Location.Name == "BlackholeSquare") {
+                    //it's a blackhole and you do these things
+
+
+                } else {
+                    //it's normal and you do these things
+                }
+            }
+
+
+        } // end Play.
+
+
+        /// <summary>
+        /// Consumes specified amount of fuel.
+        /// 
+        /// if insufficent fuel remains, fuel set to zero
+        ///  
+        /// </summary>
+        /// <param name="amount">amount of fuel used</param>
+        public void ConsumeFuel(int amount) {
+            Debug.Assert(amount > 0, "amount > 0");
+            if (fuelLeft > amount) {
+                fuelLeft -= amount;
+            } else {
+                fuelLeft = 0;
+                HasPower = false;
+            }
+        } //end ConskeFuel;
+
+
+        /// <summary>
+        ///  Checks if this player has reached the end of the game
+        /// </summary>
+        /// <returns>true if reached the Final Square</returns>
+        private bool ReachedFinalSquare() {
+
+            if (position == 55) {  // this may not be quite right - need to check
+                return true;
+            } else {
+                return false;
+            }
+
+            //return false; // so the class can compile without error
+        } //end ReachedFinalSquare
+
+
+
+    } //end class Player
+
+}
