2245acae04decd19acaf8b57a19c502d81dc2639
ellemelon <gingerellen012@gmail.com>
2018-10-07 17:30:55 +1000

    Updates to Space Race class

--- Space Race/ConsoleInterface.cs
+++ Space Race/ConsoleInterface.cs
@@ -18,11 +18,17 @@ namespace Space_Race
         static void Main(string[] args)
         {      
              DisplayIntroductionMessage();
-            /*                    
-             Set up the board in Board class (Board.SetUpBoard)
-             Determine number of players - initally play with 2 for testing purposes 
-             Create the required players in Game Logic class
-              and initialize players for start of a game             
+            //Set up the board in Board class (Board.SetUpBoard)
+            Board.SetUpBoard();
+
+            //Determine number of players - initally play with 2 for testing purposes
+            int numberOfPlayers = PromptForPlayers();
+
+            // Create the required players in Game Logic class and initialize players 
+            //for start of a game
+           SpaceRaceGame.SetUpPlayers(numberOfPlayers, SpaceRaceGame.names);
+
+            /*          
              loop  until game is finished           
                 call PlayGame in Game Logic class to play one round
                 Output each player's details at end of round
@@ -46,6 +52,31 @@ namespace Space_Race
         {
             Console.WriteLine("Welcome to Space Race.\n");
         } //end DisplayIntroductionMessage
+
+        //####################################################//
+        //the following method has been added by Ellen Morwitch
+        ///<summary>
+        /// Displays a prompt for the number of players and waits for a response.
+        /// Pre:  none
+        /// Post: the required number of players is set up and initialized for the start of a game.
+        /// </summary>
+        static int PromptForPlayers() {
+            //display message to user
+            Console.WriteLine("Please enter the number of players you wish to have (2-6): ");
+            string numPlayers = Console.ReadLine();
+
+            //check user input
+            while(int.TryParse(numPlayers, out int numOfPlayers) == false || 
+                int.Parse(numPlayers)< 2 || int.Parse(numPlayers)> 6) 
+            {
+                Console.WriteLine("That is not a valid entry, please enter a number between 2 and 6: ");
+                numPlayers = Console.ReadLine();
+            } 
+             int numberOfPlayers = int.Parse(numPlayers);
+            
+            return numberOfPlayers;
+        }//end PromptForPlayers
+
 
         /// <summary>
         /// Displays a prompt and waits for a keypress.
